8202e773c9caa8553460a372d92c83baf35a8420
KushneryukSergey <kushneriuk.ss@phystech.edu>
2020-05-04 22:52:02 +0500

    fixed __pycache__ and .idea folders removed consistent using of encode and decode returns original text

--- modules/alphabet.py
+++ modules/alphabet.py
@@ -13,19 +13,45 @@ def russian_uppercase_alphabet() -> str:
     return russian_lowercase_alphabet().upper()
 
 
+def russian_alphabet() -> str:
+    return "".join([upper_letter+lower_letter for upper_letter, lower_letter in
+                    zip(russian_uppercase_alphabet(), russian_lowercase_alphabet())])
+
+
+def english_lowercase_alphabet() -> str:
+    return string.ascii_lowercase
+
+
+def english_uppercase_alphabet() -> str:
+    return string.ascii_uppercase
+
+
+def english_alphabet() -> str:
+    return "".join([upper_letter+lower_letter for upper_letter, lower_letter in
+                    zip(english_uppercase_alphabet(), english_lowercase_alphabet())])
+
+
 def define_alphabet(language: str):
     global __ALPHABET_DEFINED
     if __ALPHABET_DEFINED:
         return
-    __ALPHABET_DEFINED = True
     global ALPHABET
     if language == "en":
-        ALPHABET = string.ascii_lowercase + string.digits + string.punctuation
+        ALPHABET = english_alphabet()
     elif language == "ru":
-        ALPHABET = russian_lowercase_alphabet() + string.digits + string.punctuation
+        ALPHABET = russian_alphabet()
     else:
         raise TypeError("Incorrect language")
+    ALPHABET += string.digits + string.punctuation
+    __ALPHABET_DEFINED = True
 
 
 def alphabet_positions() -> dict:
     return {ALPHABET[i]: i for i in range(len(ALPHABET))}
+
+
+if __name__ == "__main__":
+    print(russian_alphabet())
+    print(english_alphabet())
+    define_alphabet("en")
+    print(ALPHABET)
